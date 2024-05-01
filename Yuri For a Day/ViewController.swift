dd82bedfafed5a8e8e7210765e6aa9d65b3d9caf
チャチャ <firdakhairunnisa@gmail.com>
2024-05-01 22:34:45 +0700

    message

---
+++ Yuri For a Day/ViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var stopPressed: UIButton!
+    
+    var timer: Timer?
+    let yuriImages = ["yuri4","yuri5",  "yuri3", "yuri2", "yuri6","yuri1"]
+    let quotesImages = [#imageLiteral(resourceName: "12"), #imageLiteral(resourceName: "7"), #imageLiteral(resourceName: "8"), #imageLiteral(resourceName: "11"), #imageLiteral(resourceName: "9"), #imageLiteral(resourceName: "10")]
+   
+    // noo lol, idk what is the view
+    
+    @IBAction func startPressed(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateUI), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateUI() {
+        let image1RandomValue = UIImage(named:self.yuriImages.randomElement()!)!
+        let image2RandomValue =  self.quotesImages.randomElement()
+        
+        self.image1.image = image1RandomValue
+        self.image2.image = image2RandomValue
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: Any) {
+        timer?.invalidate()
+    }
+}
